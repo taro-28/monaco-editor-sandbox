bcf2e4536d876612828689b2935f9e2ce6f7d4bb
taro-28 <tarotaro0208@gmail.com>
2024-07-27 22:53:26 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { useEffect } from "react";
+
+const typeJsCode = `/**
+ * @typedef {Object} Data
+ * @property {string} name
+ * @property {number} age
+ */
+export {}`;
+
+const defaultValue = `/**
+ * @param { import("type").Data } data
+ */
+export default async (data) => {
+  return {
+    name: data.name,
+    age: data.age
+  };
+};;`;
 
 function App() {
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    monaco?.editor.createModel(
+      typeJsCode,
+      "javascript",
+      monaco.Uri.parse("type.js"),
+    );
+  }, [monaco]);
   return (
     <Editor
       height="90vh"
-      defaultLanguage="javascript"
-      defaultValue={`// some comment
-const x = 3;`}
+      language="javascript"
+      defaultValue={defaultValue}
+      path="main.js"
     />
   );
 }
